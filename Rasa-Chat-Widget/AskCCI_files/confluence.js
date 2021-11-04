fd78e08638c672265c697d983582a0982eeb1b3a
Bhakthi Liyanage <bhakthil@gmail.com>
2021-11-04 16:27:26 -0400

    added Chatbot-Widget

--- Rasa-Chat-Widget/AskCCI_files/confluence.js
+++
@@ -1,8 +0,0 @@
-WRMCB=function(e){var c=console;if(c&&c.log&&c.error){c.log('Error running batched script.');c.error(e);}}
-;
-try {
-/* module-key = 'confluence.extra.noemailstorm:nes-prominent-button', location = 'js/no-email-storm-prominent-button.js' */
-(function ($) {
-    $.nes_change_primary_button = true;
-})(AJS.$);
-}catch(e){WRMCB(e)};
